3ec5757cc7898f4daa2c3e9f4d15cbeb70888798
Maciej Zygadło <Xsardas665@gmail.com>
2020-11-13 18:21:19 +0100

    10679.cpp Added

---
+++ VOLUME 106/10679.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+      string s,s1;
+      int n,k;
+      scanf("%d",&n);
+      while(n--){
+            cin >> s;
+        scanf("%d",&k);
+        while(k--) {
+            string st = "";
+            cin >> s1;
+            for(int i = 0; i < s1.size() ; i++)
+                st += s[i];
+
+            if( s1 == st)
+            cout << "y" << endl;
+            else
+            cout << "n" << endl;
+        }
+      }
+
+    return 0;
+}
