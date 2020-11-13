8ebc983b9f9f661914d4759b7fda36049a5ba997
Maciej Zygadło <Xsardas665@gmail.com>
2020-11-13 18:41:00 +0100

    11172.cpp Added

---
+++ VOLUME 111/11172.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+int main ()
+{
+    int t, i;
+    long a, b;
+    scanf("%d", &t);
+    for (i=1; i<=t; i++)
+    {
+        scanf("%ld %ld", &a, &b);
+        if (a < b)
+            printf("<\n");
+        if (a > b)
+            printf(">\n");
+        if (a==b)
+            printf("=\n");
+    }
+    return 0;
+}
